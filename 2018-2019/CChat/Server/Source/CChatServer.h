568db42312cb6a5ac38b6cd77bdca6d6c36130c5
JohnRamRu <johnramru@gmail.com>
2019-03-14 11:16:39 +0500

    #5. Add "h" files for logic and GUI

---
+++ 2018-2019/CChat/Server/Source/CChatServer.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <QObject>
+#include <QTcpServer>
+#include <QTcpSocket>
+#include <QMap> 
+
+
+class CChatServer : public QObject
+{
+	Q_OBJECT
+
+private:
+	QTcpServer* server;
+	Qmap<QTcpSocket*, QString> map;
+	quint16 nextBlockSize;
+	int version;
+	
+private:
+	void sendMessageConectSlot(QString messsage); 	// пользователь подключился
+	void sendMessage(QString message); //отправить сообщение
+	void printServerInfo(QString messsage);
+	void trace();//показать трассу
+	void saveIntoTrace();//сохранить данные в трассу
+	void convertIn();//деконвертация сообщения
+	void convertOut();//конвертация сообщения
+	void kick();
+	void start(int port);
+	void stop();
+	void showMore(); // доп. окно при нажатии на клиента
+
+private slots:
+	void newClientConnectionSlot();					// слот, который срабатывает при подключении клиента
+	void readDataFromClientSlot();					// слот, который срабатывает при приеме данных от клиента
+	void startServer();
+	void stopServer();
+	void updateList();
+	void sendMessageConectSlot(QString messsage);
+	void sendMessageSlot(QString messsage);
+	void traceSlot();
+	void kickSlot();
+	void showMoreSlot();
+
+public:
+	SimpleServer(QObject *parent = 0);
+	~SimpleServer();
+};
