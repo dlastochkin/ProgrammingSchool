3f5a36a9119c47c1b70238661410d9fea3ba328f
Evgeny Ageev <johnramru@gmail.com>
2019-04-02 15:49:32 +0300

    #12 #11 Fix some files bug

--- 2018-2019/CChat/Server/Source/CChatServer.h
+++ 2018-2019/CChat/Server/Source/CChatServer.h
@@ -10,38 +10,38 @@ class CChatServer : public QObject
 {
 	Q_OBJECT
 
-private:
-	QTcpServer* server;
-	Qmap<QTcpSocket*, QString> map;
-	quint16 nextBlockSize;
-	int version;
+	private:
+		QTcpServer* server;
+		QMap<QTcpSocket*, QString> map;
+		quint16 nextBlockSize;
+		int version;
 	
-private:
-	void sendMessageConectSlot(QString messsage); 	// пользователь подключился
-	void sendMessage(QString message); //отправить сообщение
-	void printServerInfo(QString messsage);
-	void trace();//показать трассу
-	void saveIntoTrace();//сохранить данные в трассу
-	void convertIn();//деконвертация сообщения
-	void convertOut();//конвертация сообщения
-	void kick();
-	void start(int port);
-	void stop();
-	void showMore(); // доп. окно при нажатии на клиента
+	private:
+		void sendMessageConect(QString messsage); 	// пользователь подключился
+		void sendMessage(QString message); //отправить сообщение
+		void printServerInfo(QString messsage);
+		void trace();//показать трассу
+		void saveIntoTrace();//сохранить данные в трассу
+		void convertIn();//деконвертация сообщения
+		void convertOut();//конвертация сообщения
+		void kick();
+		void start(int port);
+		void stop();
+		void showMore(); // доп. окно при нажатии на клиента
 
-private slots:
-	void newClientConnectionSlot();					// слот, который срабатывает при подключении клиента
-	void readDataFromClientSlot();					// слот, который срабатывает при приеме данных от клиента
-	void startServer();
-	void stopServer();
-	void updateList();
-	void sendMessageConectSlot(QString messsage);
-	void sendMessageSlot(QString messsage);
-	void traceSlot();
-	void kickSlot();
-	void showMoreSlot();
+	private slots:
+		void newClientConnectionSlot();					// слот, который срабатывает при подключении клиента
+		void readDataFromClientSlot();					// слот, который срабатывает при приеме данных от клиента
+		void startServer();
+		void stopServer();
+		void updateList();
+		void sendMessageConectSlot(QString messsage);
+		void sendMessageSlot(QString messsage);
+		void traceSlot();
+		void kickSlot();
+		void showMoreSlot();
 
-public:
-	SimpleServer(QObject *parent = 0);
-	~SimpleServer();
+	public:
+		CChatServer(QObject *parent = 0);
+		~CChatServer();
 };
---
+++ 2018-2019/CChat/Server/Source/CChatServerGUI.cpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <QWidget.h>
+
+class ССhatServerGUI : public QObject
+{
+	Q_OBJECT
+
+	public:
+		ССhatServerGUI();
+		~ССhatServerGUI();
+
+	private:
+		void drawClientsList();
+		void drawMessageBox(); //Строка сообщения
+		void drawMessageButton(); //Кнопка сообщения
+		void drawTraceButton(); //Кнопка для вызова трассы
+		void drawStartStop(); //Кнопка для старта/остановки сервера
+		void drawServerInfo();
+		void drawMoreBox();
+		void drawChatList();
+
+	private slots:
+		void startServer();
+		void stopServer();
+		void updateList();
+		void sendMessageConectSlot(QString messsage);
+		void sendMessageSlot(QString messsage);
+		void traceSlot();
+		void kickSlot();
+		void showMoreSlot();
+
+	signals:
+		void startServerSignal();
+		void stopServerSignal();
+		void updateListSignal();
+		void sendMessageConectSignal(QString messsage);
+		void sendMessageSignal(QString messsage);
+		void traceSignal();
+		void kickSignal();
+		void showMoreSignal();
+};
