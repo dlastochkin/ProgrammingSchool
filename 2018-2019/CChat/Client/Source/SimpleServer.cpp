17bd68986c936db3ecca24b30d6ea34057a7c584
Ishbulatov Maxim <mr.rokf@mail.ru>
2019-05-07 19:23:35 +0300

    #9

---
+++ 2018-2019/CChat/Client/Source/SimpleServer.cpp
@@ -0,0 +1,186 @@
+#include "SimpleServer.h"
+
+ClientLogic::ClientLogic()
+{
+
+}
+
+void ClientLogic::start(int port, int w, int h)
+{
+	ClientGraphic* Gui = new ClientGraphic(w, h);
+	//Интерфейс коннекта
+
+	socket = new QTcpSocket(this);
+	socket->connectToHost(Gui->port, Gui->host);
+
+
+}
+
+/*
+void ClientLogic::readDataFromServerSlot()
+{
+	QDataStream in(socket);
+	while (true)
+	{
+		if (!nextBlockSize)
+		{
+			if (socket->bytesAvailable() < sizeof(quint16))
+			{
+				break;
+			}
+			in >> nextBlockSize;
+		}
+
+		if (socket->bytesAvailable() < nextBlockSize)
+		{
+			break;
+		}
+
+		QString serverMessage;
+		in >> serverMessage;
+		qDebug() << "Server send message: " << serverMessage << "\n";
+
+		nextBlockSize = 0;
+	}
+}
+*/
+
+void ClientLogic::sendVersion(QString version)
+{
+	QByteArray messageBytes;
+	QDataStream out(&messageBytes, QIODevice::WriteOnly);
+	
+		out << VERSION << quint32(0) << MinorBite << MajorBite;//MinorBite and MajorBite quint16
+		out.device()->seek(1);
+		out << quint32(messageBytes.size() - sizeof(quint32) - sizeof(quint8));
+		socket->write(messageBytes);
+}
+
+void  ClientLogic::clientConnected(QString* name)
+{
+	QByteArray messageBytes;
+	QDataStream out(&messageBytes, QIODevice::WriteOnly);
+
+	out << CLIENT_CONNECTION << quint32(0) <<name;//MinorBite and MajorBite quint16
+	out.device()->seek(1);
+	out << quint32(messageBytes.size() - sizeof(quint32) - sizeof(quint8));
+	socket->write(messageBytes);
+}
+
+void ClientLogic::privateMessage(QString message)
+{
+	QByteArray messageBytes;
+	QDataStream out(&messageBytes, QIODevice::WriteOnly);
+
+	out << PRIVATE_MESSAGE << quint32(0) << message;//MinorBite and MajorBite quint16
+	out.device()->seek(1);
+	out << quint32(messageBytes.size() - sizeof(quint32) - sizeof(quint8));
+	socket->write(messageBytes);
+}
+
+void ClientLogic::chatMessage(QString* message)
+{
+	QByteArray messageBytes;
+	QDataStream out(&messageBytes, QIODevice::WriteOnly);
+
+	out << CHAT_MESSAGE << quint32(0) << message;//MinorBite and MajorBite quint16
+	out.device()->seek(1);
+	out << quint32(messageBytes.size() - sizeof(quint32) - sizeof(quint8));
+	socket->write(messageBytes);
+
+}
+
+void ClientLogic::clientDisconnected()
+{
+	QByteArray messageBytes;
+	QDataStream out(&messageBytes, QIODevice::WriteOnly);
+
+	out << CLIENT_DISCONNECTED;
+	socket->write(messageBytes);
+}
+
+void ClientLogic::keepAliveRS()
+{
+	QByteArray messageBytes;
+	QDataStream out(&messageBytes, QIODevice::WriteOnly);
+
+	out << KEEP_ALIVE_RS;
+	socket->write(messageBytes);
+}
+
+void ClientLogic::recieveMessage()
+{
+	quint8 type;
+	quint32 lenght;
+	QDataStream in(socket);
+	while (true)
+	{
+		if (!type)
+		{
+			if (socket->bytesAvailable() < sizeof(quint8))
+			{
+				break;
+			}
+			in >> type;
+		}
+
+		if (!lenght)
+		{
+			if (socket->bytesAvailable() < sizeof(quint32))
+			{
+				break;
+			}
+			in >> lenght;
+		}
+
+		if (socket->bytesAvailable() < lenght)
+		{
+			break;
+		}
+
+		switch (type)
+		{
+		case VERSION:
+		{
+
+		}
+		case CLIENTS_LIST:
+		{
+			int i = 0;
+			QString Message;
+			in >> Message;
+			clientList = Message.split("/0");
+		}
+		case PRIVATE_MESSAGE:
+		{
+			QString Message;
+			in >> Message;
+			QStringList Messages;
+			PrivateCurrentName = Messages.takeFirst();
+			PrivateCurrentMessage = Messages.takeLast();
+
+		}
+		case CHAT_MESSAGE:
+		{
+			QString Message;
+			in >> Message;
+			QStringList Messages;
+			ChatCurrentName = Messages.takeFirst();
+			ChatCurrentMessage = Messages.takeLast();
+		}
+		case KICK:
+		{
+			QString kickReason;
+			in >> kickReason;
+
+		}
+		case SERVER_SHUTDOWN:
+		{
+
+		}
+		};
+
+		lenght = 0;
+		type = 0;
+	}
+}
---
+++ 2018-2019/CChat/Client/Source/SimpleServer.h
@@ -0,0 +1,69 @@
+#pragma once
+#include <QObject>
+#include <QTcpServer>
+#include <QTcpSocket>
+#include <iostream>
+#include <QString>
+#include <QDataStream>
+#include <string>
+#include "ClientGraphic.h"
+
+
+#define VERSION				0x1
+#define CLIENT_CONNECTION	0x2
+#define CLIENTS_LIST		0X3
+#define PRIVATE_MESSAGE		0X4
+#define CHAT_MESSAGE		0X5
+#define KICK				0X6
+#define CLIENT_DISCONNECTED	0X7
+#define SERVER_SHUTDOWN		0X8
+#define KEEP_ALIVE_RQ		0X9
+#define KEEP_ALIVE_RS		0XA
+
+class ClientLogic : public QObject
+{
+	Q_OBJECT
+private:
+	quint16 MinorBite;
+	quint16 MajorBite;
+	QString Name;
+	void sendVersion(QString version);
+	QTcpServer* server;
+	QTcpSocket* socket = nullptr;
+	quint16 nextBlockSize;
+	QStringList clientList;
+	QStringList mainChat;
+	//PrivateMessage
+	//==============================
+	QString PrivateCurrentName;
+	QString PrivateCurrentMessage;
+	//ChatMessage
+	//==============================
+	QString ChatCurrentName;
+	QString ChatCurrentMessage;
+	//==============================
+	QString kickReason;
+
+private:
+	//SEND_MESSAGE
+	void sendVersion(QString version);	// отправка сообщения "medved" клиенту
+	void clientConnected(QString* name);
+	void privateMessage(QString message);
+	void chatMessage(QString* message);
+	void clientDisconnected();
+	void keepAliveRS();
+	//RECIEVE_MESSAGE
+	void recieveMessage();
+
+public slots:
+	void newClientConnectionSlot();					// слот, который срабатывает при подключении клиента
+	void readDataFromServerSlot();					// слот, который срабатывает при приеме данных от клиента
+	
+public:
+	ClientLogic();
+	~ClientLogic();
+
+	void start(int port, int w, int h);
+
+	void stop();
+};
